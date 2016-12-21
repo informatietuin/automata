2600bc6445f2dfa7b67891fab55cebe1bb973c08
Remko Boschker <remkoboschker@RN-145-97-130-70.eduroam.rug.nl>
2016-12-21 14:10:53 +0100

    adds option to return distance and option for case insensitive matching

--- src/lev.js
+++ src/lev.js
@@ -1,36 +1,49 @@
 module.exports = function lev() {
-  function recLev(max, state, strM, str) {
-    // if there are chars left in the string to match
-    if (strM.length > 0) {
-      const newState = [state[0] + 1]; // the first entry is the match from an empty string
-      for (let i = 0; i < state.length - 1; i += 1) {
-        const deletion = newState[i] + 1;
-        const insertion = state[i + 1] + 1;
-        const cost = strM.charAt() === str.charAt(i) ? 0 : 1;
-        const substitution = state[i] + cost;
-        const value = Math.min(deletion, insertion, substitution);
+  function recLev(max, state, strM, str, mode = 'object', caseSensitive = true) {
+    // the first entry is the match from an empty string
+    const newState = [state[0] + 1];
 
-        newState.push(value);
+    // if there are no chars left in the string to match
+    if (strM.length === 0) {
+      const distance = state.pop();
+      const pass = distance <= max;
+      if (mode === 'bool') {
+        return pass;
       }
+      return { distance, pass };
+    }
 
-      // if you can still match, check the next char
-      if (Math.min(...newState) <= max) {
-        return recLev(max, newState, strM.slice(1), str);
-      }
+    for (let i = 0; i < state.length - 1; i += 1) {
+      const deletion = newState[i] + 1;
+      const insertion = state[i + 1] + 1;
+      const cost = (caseSensitive
+        ? strM.charAt(0) === str.charAt(i)
+        : strM.charAt(0).toLowerCase() === str.charAt(i).toLowerCase())
+          ? 0 : 1;
+      const substitution = state[i] + cost;
+      const value = Math.min(deletion, insertion, substitution);
 
-      // if you can't match, return false
+      newState.push(value);
+    }
+
+    if (Math.min(...newState) > max) {
       return false;
     }
-    // no more chars, is the last entry with the distance?
-    return state.pop() <= max;
+
+    return recLev(max, newState, strM.slice(1), str, mode, caseSensitive);
   }
 
-  function match(max, strToMatch, str) {
+  function match({ max, strToMatch, str, mode, caseSensitive }) {
     const firstRow = [...new Array(str.length + 1).keys()];
-    return recLev(max, firstRow, strToMatch, str);
+    return recLev(max, firstRow, strToMatch, str, mode, caseSensitive);
+  }
+
+  function test({ max, strToMatch, str, caseSensitive }) {
+    return match({ max, strToMatch, str, mode: 'bool', caseSensitive });
   }
 
   return Object.freeze({
+    test,
     match,
   });
 };
--- test/test-lev.js
+++ test/test-lev.js
@@ -4,23 +4,33 @@ const expect = require('chai').expect;
 describe('lev', () => {
   describe('match', () => {
     it('should handle empty strings', () => {
-      expect(lev().match(1, 'a', '')).to.be.true;
-      expect(lev().match(1, '', 'a')).to.be.true;
+      expect(lev().test({ max: 1, strToMatch: 'a', str: '' })).to.be.true;
+      expect(lev().test({ max: 1, strToMatch: '', str: 'a' })).to.be.true;
     });
     it('should handle a distace of zero', () => {
-      expect(lev().match(0, 'a', 'a')).to.be.true;
-      expect(lev().match(0, '', 'a')).to.be.false;
+      expect(lev().test({ max: 0, strToMatch: 'a', str: 'a' })).to.be.true;
+      expect(lev().test({ max: 0, strToMatch: '', str: 'a' })).to.be.false;
     });
     it('should fail on edit distance too long', () => {
-      expect(lev().match(1, 'a', 'aaa')).to.be.false;
+      expect(lev().test({ max: 1, strToMatch: 'a', str: 'aaa' })).to.be.false;
     });
     it('should handle deletion', () => {
-      expect(lev().match(1, 'abc', 'ac')).to.be.true;
-      expect(lev().match(1, 'abc', 'ad')).to.be.false;
+      expect(lev().test({ max: 1, strToMatch: 'abc', str: 'ac' })).to.be.true;
+      expect(lev().test({ max: 1, strToMatch: 'abc', str: 'ad' })).to.be.false;
     });
     it('should handle substitution', () => {
-      expect(lev().match(1, 'abc', 'aac')).to.be.true;
-      expect(lev().match(1, 'abc', 'aad')).to.be.false;
+      expect(lev().test({ max: 1, strToMatch: 'abc', str: 'aac' })).to.be.true;
+      expect(lev().test({ max: 1, strToMatch: 'abc', str: 'aad' })).to.be.false;
+    });
+    it('should respect the mode set to bool or not', function () {
+      expect(lev().match({ max: 1, strToMatch: 'a', str: 'a' })).to.deep.equal({
+        pass: true, distance: 0,
+      });
+      expect(lev().match({ max: 1, strToMatch: 'a', str: 'a', mode: 'bool' })).to.be.true;
+    });
+    it('should respect the caseSenstivity setting', function() {
+      expect(lev().test({ max: 1, strToMatch: 'aBC', str: 'abc' })).to.be.false;
+      expect(lev().test({ max: 1, strToMatch: 'aBC', str: 'abc', caseSensitive: false })).to.be.true;
     });
   });
 });
